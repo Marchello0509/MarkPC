9b13cbeedaecee20a3e1fc383bf8509ee7a43de3
Marchello <129538566+Marchello0509@users.noreply.github.com>
2023-11-03 23:01:04 +0200

    Login (Sv1)

---
+++ Js/login.js
@@ -0,0 +1,39 @@
+const htmlEl = {
+    inpEmail: document.querySelector('#inpEmail'),
+    inpPass: document.querySelector('#inpPass'),
+    btnLog: document.querySelector('#btnLog'),
+    btnGoogle: document.querySelector('#btnGoogle'),
+    block: document.querySelector('#block')
+}
+
+let dataBase = [
+    {
+        email: "marchello0509@gmail.com",
+        password: "Admin0509"
+    }
+]
+
+htmlEl.btnLog.addEventListener('click', () => {
+    const userEmail = htmlEl.inpEmail.value
+    const userPass = htmlEl.inpPass.value
+    dataBase.forEach((user) => {
+        if (userEmail === user.email && userPass === user.password) {
+            alert("Welcome, Mark!")
+            console.log(true)
+            htmlEl.block.style.backgroundColor = "rgb(58, 250, 0)";
+            setTimeout(() => {
+                htmlEl.block.style.backgroundColor = "#F5F1F1";
+            }, 1000);
+        } else {
+            console.log(false)
+            htmlEl.block.style.backgroundColor = "rgb(250, 33, 0)";
+            setTimeout(() => {
+                htmlEl.block.style.backgroundColor = "#F5F1F1";
+            }, 1000);
+        }
+    })
+})
+
+htmlEl.btnGoogle.addEventListener('click', () => {
+    window.location.href = 'https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fmyaccount.google.com%2F&ec=GAlAwAE&hl=ru&service=accountsettings&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S1276044089%3A1698749451300939&theme=glif';
+}) 
